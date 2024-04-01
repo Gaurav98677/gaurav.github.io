e6354eb3c87c3904b18ee4cf651683129d1d0b4a
Gaurav98677 <156471613+Gaurav98677@users.noreply.github.com>
2024-04-01 19:24:55 -0400

    update 4.js
    in result() function, uses a newStory.replaceAll() function call to replace :insertx:, :inserty: and :insertz: with the random insertX, insertY and insertZ variables
    in result function(), if a user enters a name in the cutomname text input, replacces "Bob" in the newstory variable with the name entered

--- a4p1/main.js
+++ a4p1/main.js
@@ -24,8 +24,14 @@ function result() {
     const yItem = randomValueFromArray(insertY);
     const zItem = randomValueFromArray(insertZ);
 
+    newStory = newStory.replaceAll(':insertx:',xItem);
+    newStory = newStory.replaceAll(':inserty:',yItem);
+    newStory = newStory.replaceAll(':insertz:',zItem);
+
+
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replaceAll('Bob', name);
 
   }
 
