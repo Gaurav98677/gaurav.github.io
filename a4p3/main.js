e7541de0c93ab96ed8a1974b409084214e50723c
Gaurav98677 <156471613+Gaurav98677@users.noreply.github.com>
2024-04-01 22:11:25 -0400

    update5.js
    implemented the provided collisionDetect() function that will allow collision detection and included a collsionDetect() function call in the loop() function
    Files published implemented most of the requirements

--- a4p3/main.js
+++ a4p3/main.js
@@ -84,4 +84,19 @@ function loop() {
   requestAnimationFrame(loop);
 }
 
+
+
+Ball.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+      }
+    }
+  }
+}  
 loop(); 
