becf3fa9e496706162e1c0ac52f7fbe17f1477a8
Gaurav98677 <156471613+Gaurav98677@users.noreply.github.com>
2024-04-01 23:06:38 -0400

    update5.js
    implemented in the EvilCircle class a method named collisionDetect() that complies with the requirements

--- a4p4/main-finished.js
+++ a4p4/main-finished.js
@@ -136,6 +136,26 @@ class EvilCircle extends Shape {
   }
 
 
+  
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          para.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
+
+}
+
+
 const balls = [];
 
 while (balls.length < 25) {
