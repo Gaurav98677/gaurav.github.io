313cee95607c0324bd49cb49e7cb3cc845eda461
Gaurav98677 <156471613+Gaurav98677@users.noreply.github.com>
2024-04-01 21:05:26 -0400

    update 3.js

--- a4p2/main.js
+++ a4p2/main.js
@@ -5,6 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+/* Declaring the alternative text for each image file */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
   'pic1.jpg' : 'a human eye',
@@ -14,7 +15,11 @@ const alts = {
   'pic5.jpg' : 'a Large moth'
 }
 
-/* Declaring the alternative text for each image file */
+
+
+
+/* Looping through images */
+
 for (const image of images) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/${image}`);
@@ -25,12 +30,5 @@ for (const image of images) {
       displayedImage.alt = e.target.alt;
     });
   }
-
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
